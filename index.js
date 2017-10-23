172c010f28a1302e7e3f258cd3fa414deaf48463
Sowmya <r.a.sowmya@capgemini.com>
2017-10-23 16:33:40 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       Hi............
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
